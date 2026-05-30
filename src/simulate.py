cc6062e974016530cf3ba5875cef7984166056ac
Aidan Lowe <102561929+aidanlowe21@users.noreply.github.com>
2026-05-30 16:21:46 -0400

    Update simulate.py

--- src/simulate.py
+++ src/simulate.py
@@ -4,9 +4,13 @@ from kalman import recover_gap_kf, get_kf_params
 from particle import get_pf_params, recover_gap_pf
 
 def simulate(T: int, n_sims: int, x_true: np.ndarray, y_true: np.ndarray) -> dict[str, np.ndarray]:
-    mle_sim = np.zeros((n_sims, T))
-    kf_sim = np.zeros((n_sims, T))
-    pf_sim = np.zeros((n_sims, T))
+    mle_point = np.zeros((n_sims, T))
+    kf_point = np.zeros((n_sims, T))
+    pf_point = np.zeros((n_sims, T))
+
+    mle_var = np.zeros((n_sims, T))
+    kf_var = np.zeros((n_sims, T))
+    pf_var = np.zeros((n_sims, T))
 
     param_keys = ['phi', 'alpha', 'sigma2', 'tau2']
     mle_params_all = np.zeros((n_sims, 4))
@@ -15,18 +19,12 @@ def simulate(T: int, n_sims: int, x_true: np.ndarray, y_true: np.ndarray) -> dic
 
     for i in range(n_sims):
         mle_params = mle_estimate(x_true[i], y_true[i])
-        kf_params = get_kf_params(y_true[i],
-                                 initial_gap_guess=y_true[i][0],
-                                 initial_gap_var=0.05
-                                 )
+        kf_params = get_kf_params(y_true[i])
         pf_params = get_pf_params(y_true[i])
 
-        mle_sim[i] = recover_gap_mle(mle_params, y_true[i])[0]
-        kf_sim[i] = recover_gap_kf(kf_params, y_true[i],
-                                  initial_gap_guess=y_true[i][0],
-                                  initial_gap_var=0.05
-                                  )[0]
-        pf_sim[i] = recover_gap_pf(pf_params, y_true[i])[0]
+        mle_point[i], mle_var[i] = recover_gap_mle(mle_params, y_true[i])
+        kf_point[i], kf_var[i] = recover_gap_kf(kf_params, y_true[i])
+        pf_point[i], pf_var[i] = recover_gap_pf(pf_params, y_true[i])
 
         mle_params_all[i] = [mle_params[k] for k in param_keys]
         kf_params_all[i] = kf_params
